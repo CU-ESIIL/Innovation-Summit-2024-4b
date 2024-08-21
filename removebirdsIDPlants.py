58c7957d978fa03dd51ab970a88eae0ec3e94682
miguelcleon <miguel.leon@unh.edu>
2024-08-21 09:33:01 -0400

    use COCO class labels that make more sense for this application
    use COCO class labels that make more sense for this application

--- removebirdsIDPlants.py
+++ removebirdsIDPlants.py
@@ -17,18 +17,15 @@ api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_
 
 # Load the COCO class labels
 COCO_CLASSES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag',
-    'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite',
-    'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana',
-    'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table',
-    'toilet', 'TV', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
-    'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+    '__background__', 'person', 'bird', 'tree', 'bush', 'grass', 'flower',
+    'rock', 'stream', 'river', 'lake', 'mountain', 'sky', 'cloud',
+    'butterfly', 'insect', 'squirrel', 'deer', 'rabbit', 'frog',
+    'snake', 'fish', 'mushroom', 'leaf', 'log', 'path',
+    'binoculars', 'camera', 'backpack', 'hat', 'walking stick',
+    'tent', 'campfire', 'kayak', 'canoe', 'birdhouse', 'hummingbird',
+    'feeder', 'nest', 'birdbath', 'birdwatching platform',
+    'bird identification book', 'scope', 'bird call', 'waterfall',
+    'pond', 'branch', 'twig', 'fence', 'trail sign', 'sand', 'shell'
 ]
 
 # Load a pre-trained segmentation model
